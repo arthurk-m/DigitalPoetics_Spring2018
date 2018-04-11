e0f951b1dedf0b3f56e9db59ec1477f28aa09bc7
Arthur Kim <akim15@risd.edu>
2018-04-11 19:01:25 -0400

    push this

---
+++ TAVIT_POEM/script.js
@@ -0,0 +1,363 @@
+$(document).ready(function(){
+
+
+  var audio = $("#love")[0];
+
+// $("#trust2").mouseenter(function() {
+//   $("#love").play();
+// });
+
+
+    $('#humancondition')
+	.mouseover(function() {
+		$('body').css('background', 'url(https://media.giphy.com/media/63erJDAX1rYE8/giphy.gif) no-repeat center center fixed');
+        $('body').css('background-size', 'cover');
+    })
+    .mouseout(function() {
+        $('body').css('background', '');
+        $('body').css('background-size', '');
+  });
+  //
+  $('#beauty')
+	.mouseover(function() {
+		$('body').css('background', 'url(https://media.giphy.com/media/63erJDAX1rYE8/giphy.gif) no-repeat center center fixed');
+        $('body').css('background-size', 'cover');
+    })
+    .mouseout(function() {
+        $('body').css('background', '');
+        $('body').css('background-size', '');
+  });
+  //
+  $('#humans1')
+	.mouseover(function() {
+		$('body').css('background', 'url(https://media.giphy.com/media/3oxRmhpsOgxoSeJiz6/giphy.gif) no-repeat center center fixed');
+        $('body').css('background-size', 'cover');
+    })
+    .mouseout(function() {
+        $('body').css('background', '');
+        $('body').css('background-size', '');
+  });
+  //
+  $('#complex')
+	.mouseover(function() {
+		$('body').css('background', 'url(https://media.giphy.com/media/11E50BmTqrXG0/giphy.gif) no-repeat center center fixed');
+        $('body').css('background-size', 'cover');
+    })
+    .mouseout(function() {
+        $('body').css('background', '');
+        $('body').css('background-size', '');
+  });
+  //
+  $('#trying')
+	.mouseover(function() {
+		$('body').css('background', 'url(https://media.giphy.com/media/13ywPzPJdfhmBG/giphy.gif) no-repeat center center fixed');
+        $('body').css('background-size', 'cover');
+    })
+    .mouseout(function() {
+        $('body').css('background', '');
+        $('body').css('background-size', '');
+  });
+  //
+  $('#compass')
+	.mouseover(function() {
+		$('body').css('background', 'url(https://media.giphy.com/media/BCIRKxED2Y2JO/giphy.gif) no-repeat center center fixed');
+        $('body').css('background-size', 'cover');
+    })
+    .mouseout(function() {
+        $('body').css('background', '');
+        $('body').css('background-size', '');
+  });
+  //
+  $('#good1')
+	.mouseover(function() {
+		$('body').css('background', 'url(https://media.giphy.com/media/l0MYxef0mpdcnQnvi/giphy.gif) no-repeat center center fixed');
+        $('body').css('background-size', 'cover');
+    })
+    .mouseout(function() {
+        $('body').css('background', '');
+        $('body').css('background-size', '');
+  });
+  //
+  $('#evil1')
+	.mouseover(function() {
+		$('body').css('background', 'url(https://media.giphy.com/media/yr7n0u3qzO9nG/giphy.gif) no-repeat center center fixed');
+        $('body').css('background-size', 'cover');
+    })
+    .mouseout(function() {
+        $('body').css('background', '');
+        $('body').css('background-size', '');
+  });
+  //
+  $('#good2')
+	.mouseover(function() {
+		$('body').css('background', 'url(https://media.giphy.com/media/11ISwbgCxEzMyY/giphy.gif) no-repeat center center fixed');
+        $('body').css('background-size', 'cover');
+    })
+    .mouseout(function() {
+        $('body').css('background', '');
+        $('body').css('background-size', '');
+  });
+  //
+  $('#evil2')
+	.mouseover(function() {
+		$('body').css('background', 'url(https://media.giphy.com/media/db3pcpCCNsUb6/giphy.gif) no-repeat center center fixed');
+        $('body').css('background-size', 'cover');
+    })
+    .mouseout(function() {
+        $('body').css('background', '');
+        $('body').css('background-size', '');
+  });
+  //
+  $('#introvert')
+	.mouseover(function() {
+		$('body').css('background', 'url(https://media.giphy.com/media/xT0GqeV6pCFWI6fw8E/giphy.gif) no-repeat center center fixed');
+        $('body').css('background-size', 'cover');
+    })
+    .mouseout(function() {
+        $('body').css('background', '');
+        $('body').css('background-size', '');
+  });
+  //
+  $('#extrovert')
+	.mouseover(function() {
+		$('body').css('background', 'url(https://media.giphy.com/media/1i5W4BWgTdza/giphy.gif) no-repeat center center fixed');
+        $('body').css('background-size', 'cover');
+    })
+    .mouseout(function() {
+        $('body').css('background', '');
+        $('body').css('background-size', '');
+  });
+  //
+  $('#binary')
+	.mouseover(function() {
+		$('body').css('background', 'url(https://media.giphy.com/media/3ov9jQqccWIOQzjD7q/giphy.gif) no-repeat center center fixed');
+        $('body').css('background-size', 'cover');
+    })
+    .mouseout(function() {
+        $('body').css('background', '');
+        $('body').css('background-size', '');
+  });
+  //
+  $('#spectrum')
+	.mouseover(function() {
+		$('body').css('background', 'url(https://media.giphy.com/media/v31tiSUexQRHi/giphy.gif) no-repeat center center fixed');
+        $('body').css('background-size', 'cover');
+    })
+    .mouseout(function() {
+        $('body').css('background', '');
+        $('body').css('background-size', '');
+  });
+  //
+  $('#introvert')
+	.mouseover(function() {
+		$('body').css('background', 'url(https://media.giphy.com/media/xT0GqeV6pCFWI6fw8E/giphy.gif) no-repeat center center fixed');
+        $('body').css('background-size', 'cover');
+    })
+    .mouseout(function() {
+        $('body').css('background', '');
+        $('body').css('background-size', '');
+  });
+  //
+  $('#extrovert')
+	.mouseover(function() {
+		$('body').css('background', 'url(https://media.giphy.com/media/1i5W4BWgTdza/giphy.gif) no-repeat center center fixed');
+        $('body').css('background-size', 'cover');
+    })
+    .mouseout(function() {
+        $('body').css('background', '');
+        $('body').css('background-size', '');
+  });
+  //
+  $('#ambivert')
+	.mouseover(function() {
+		$('body').css('background', 'url(https://media.giphy.com/media/111LSMM1ICPqBq/giphy.gif) no-repeat center center fixed');
+        $('body').css('background-size', 'cover');
+    })
+    .mouseout(function() {
+        $('body').css('background', '');
+        $('body').css('background-size', '');
+  });
+  //
+  $('#categorize')
+	.mouseover(function() {
+		$('body').css('background', 'url(https://media.giphy.com/media/8wMpGui0SjfmE/giphy.gif) no-repeat center center fixed');
+        $('body').css('background-size', 'cover');
+    })
+    .mouseout(function() {
+        $('body').css('background', '');
+        $('body').css('background-size', '');
+  });
+  //
+  $('#understand')
+	.mouseover(function() {
+		$('body').css('background', 'url(https://media.giphy.com/media/sM4ALgO3D7F8k/giphy.gif) no-repeat center center fixed');
+        $('body').css('background-size', 'cover');
+    })
+    .mouseout(function() {
+        $('body').css('background', '');
+        $('body').css('background-size', '');
+  });
+  //
+  $('#lonely')
+	.mouseover(function() {
+		$('body').css('background', 'url(https://media.giphy.com/media/VfyC5j7sR4cso/giphy.gif) no-repeat center center fixed');
+        $('body').css('background-size', 'cover');
+    })
+    .mouseout(function() {
+        $('body').css('background', '');
+        $('body').css('background-size', '');
+  });
+  //
+  $('#social')
+	.mouseover(function() {
+		$('body').css('background', 'url(https://media.giphy.com/media/1zKdb4WSHgY4QKAsjo/giphy.gif) no-repeat center center fixed');
+        $('body').css('background-size', 'cover');
+    })
+    .mouseout(function() {
+        $('body').css('background', '');
+        $('body').css('background-size', '');
+  });
+  //
+  $('#trust')
+	.mouseover(function() {
+		$('body').css('background', 'url(https://media.giphy.com/media/Jz75B6BpVG728/giphy.gif) no-repeat center center fixed');
+        $('body').css('background-size', 'cover');
+    })
+    .mouseout(function() {
+        $('body').css('background', '');
+        $('body').css('background-size', '');
+  });
+  //
+  $('#trust2')
+	.mouseover(function() {
+		$('body').css('background', 'url(https://media.giphy.com/media/TY7dLV4kDmZSU/giphy.gif) no-repeat center center fixed');
+        $('body').css('background-size', 'cover');
+        audio.play();
+    })
+    .mouseout(function() {
+        $('body').css('background', '');
+        $('body').css('background-size', '');
+  });
+  //
+  $('#shape')
+	.mouseover(function() {
+		$('body').css('background', 'url(https://media.giphy.com/media/3ohhwHsTiXnRe7wsla/giphy.gif) no-repeat center center fixed');
+        $('body').css('background-size', 'cover');
+    })
+    .mouseout(function() {
+        $('body').css('background', '');
+        $('body').css('background-size', '');
+  });
+  //
+  $('#mold')
+	.mouseover(function() {
+		$('body').css('background', 'url(https://media.giphy.com/media/10rzSQkQVYlBHa/giphy.gif) no-repeat center center fixed');
+        $('body').css('background-size', 'cover');
+    })
+    .mouseout(function() {
+        $('body').css('background', '');
+        $('body').css('background-size', '');
+  });
+  //
+  $('#world')
+	.mouseover(function() {
+		$('body').css('background', 'url(https://media.giphy.com/media/l3V0megwbBeETMgZa/giphy.gif) no-repeat center center fixed');
+        $('body').css('background-size', 'cover');
+    })
+    .mouseout(function() {
+        $('body').css('background', '');
+        $('body').css('background-size', '');
+  });
+  //
+  $('#decisions')
+	.mouseover(function() {
+		$('body').css('background', 'url(https://media.giphy.com/media/5wBBmDD10iAg/giphy.gif) no-repeat center center fixed');
+        $('body').css('background-size', 'cover');
+    })
+    .mouseout(function() {
+        $('body').css('background', '');
+        $('body').css('background-size', '');
+  });
+  //
+  $('#exist')
+	.mouseover(function() {
+		$('body').css('background', 'url(https://media.giphy.com/media/H9EwUTVsIqcj6/giphy.gif) no-repeat center center fixed');
+        $('body').css('background-size', 'cover');
+    })
+    .mouseout(function() {
+        $('body').css('background', '');
+        $('body').css('background-size', '');
+  });
+  //
+  $('#global')
+	.mouseover(function() {
+		$('body').css('background', 'url(https://media.giphy.com/media/l0HlMURBbyUqF0XQI/giphy.gif) no-repeat center center fixed');
+        $('body').css('background-size', 'cover');
+    })
+    .mouseout(function() {
+        $('body').css('background', '');
+        $('body').css('background-size', '');
+  });
+  //
+  $('#compass2')
+	.mouseover(function() {
+		$('body').css('background', 'url(https://media.giphy.com/media/oY0KOHdmCkiJi/giphy.gif) no-repeat center center fixed');
+        $('body').css('background-size', 'cover');
+    })
+    .mouseout(function() {
+        $('body').css('background', '');
+        $('body').css('background-size', '');
+  });
+  //
+  $('#we')
+	.mouseover(function() {
+		$('body').css('background', 'url(https://media.giphy.com/media/5zcr0oacuhvK8/giphy.gif) no-repeat center center fixed');
+        $('body').css('background-size', 'cover');
+    })
+    .mouseout(function() {
+        $('body').css('background', '');
+        $('body').css('background-size', '');
+  });
+  //
+  $('#questions')
+	.mouseover(function() {
+		$('body').css('background', 'url(https://media.giphy.com/media/3o7buirYcmV5nSwIRW/giphy.gif) no-repeat center center fixed');
+        $('body').css('background-size', 'cover');
+    })
+    .mouseout(function() {
+        $('body').css('background', '');
+        $('body').css('background-size', '');
+  });
+  //
+  $('#individualism')
+	.mouseover(function() {
+		$('body').css('background', 'url(https://media.giphy.com/media/13GIAl4R21YLgQ/giphy.gif) no-repeat center center fixed');
+        $('body').css('background-size', 'cover');
+    })
+    .mouseout(function() {
+        $('body').css('background', '');
+        $('body').css('background-size', '');
+  });
+  //
+  $('#easiness')
+	.mouseover(function() {
+		$('body').css('background', 'url(https://media.giphy.com/media/c1TiXpGGO8JDW/giphy.gif) no-repeat center center fixed');
+        $('body').css('background-size', 'cover');
+    })
+    .mouseout(function() {
+        $('body').css('background', '');
+        $('body').css('background-size', '');
+  });
+  //
+  $('#constructed')
+	.mouseover(function() {
+		$('body').css('background', 'url(https://media.giphy.com/media/3o7abDnUPDkOC05QU8/giphy.gif) no-repeat center center fixed');
+        $('body').css('background-size', 'cover');
+    })
+    .mouseout(function() {
+        $('body').css('background', '');
+        $('body').css('background-size', '');
+  });
+  //
+
+});
